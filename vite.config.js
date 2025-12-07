29aa02be7a0ca490c3516bd867efce65fe851c06
AlexanderArj <aromerojuliao@byupathway.edu>
2025-12-06 22:46:10 -0300

    Working with Netlify pages only

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/chesspuzzles/",
+  base: "/",
   server: { port: 3000 },
   preview: { port: 4173 },
   build: { outDir: "dist" }
